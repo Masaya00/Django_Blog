4734b1733af2d5ff4657e079498a2b1d310dae38
Masaya00 <masaya.asxce@icloud.com>
2021-01-07 02:28:30 +0900

    top page

--- dockerblog/settings.py
+++ dockerblog/settings.py
@@ -110,6 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# メディアファイルを管理
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
+MEDIA_URL = '/media/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
